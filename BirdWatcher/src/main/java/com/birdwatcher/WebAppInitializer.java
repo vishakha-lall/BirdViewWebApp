da1f02659504fdaa2771a6575fad6af85f99ee8b
vishakha-lall <vishakha.lall@outlook.com>
2017-09-28 12:08:58 +0530

    Image sent to server in upload-dir

--- BirdWatcher/src/main/java/com/birdwatcher/WebAppInitializer.java
+++ BirdWatcher/src/main/java/com/birdwatcher/WebAppInitializer.java
@@ -1,17 +1,31 @@
 package com.birdwatcher;
 
+import javax.annotation.Resource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import com.birdwatcher.storage.StorageService;
+
+@SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan("com.birdwatcher")
 public class WebAppInitializer{
-
+	
+	@Resource
+	StorageService storageService; 
+	
     public static void main(String[] args) throws Exception{
         SpringApplication.run(WebAppInitializer.class, args);
     }
+    
+    public void run(String... args) throws Exception {
+   		storageService.deleteAll();
+   		storageService.init();
+   	}
 }
 
--- BirdWatcher/src/main/java/com/birdwatcher/controller/UploadController.java
+++ BirdWatcher/src/main/java/com/birdwatcher/controller/UploadController.java
@@ -1,21 +1,42 @@
 package com.birdwatcher.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
-import com.birdwatcher.model.UserDTO;
+import com.birdwatcher.storage.StorageService;
 
 @Controller
-@RequestMapping(value="/upload")
 public class UploadController {
-	@RequestMapping(method = RequestMethod.POST)
+
+	@Autowired
+	StorageService storageService;
+
+	List<String> files = new ArrayList<String>();
 	@ResponseBody
-    public String authenticate(@RequestBody MultipartFile img){
-		return img.toString();
-    }
+	@RequestMapping(value="/upload",method = RequestMethod.POST)
+	public String handleFileUpload(@RequestParam("file") MultipartFile file, Model model) {
+		try {
+			storageService.store(file);
+			model.addAttribute("message", "You successfully uploaded " + file.getOriginalFilename() + "!");
+			files.add(file.getOriginalFilename());
+		} catch (Exception e) {
+			model.addAttribute("message", "FAIL to upload " + file.getOriginalFilename() + "!");
+		}
+		return "uploadForm";
+	}
 }
